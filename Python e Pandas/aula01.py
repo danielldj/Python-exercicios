1bd780b64e06d80888672ddb23bc3f414f971d8f
Daniel Sequeira <142548967+danielldj@users.noreply.github.com>
2024-05-25 11:06:13 -0300

    Delete Python e Pandas directory

--- Python e Pandas/aula01.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-vendas_df = pd.read_csv('Contoso - Vendas  - 2017.csv', sep=';')
-lojas_df = pd.read_csv('Contoso - Lojas',sep=';')
-produtos_df = pd.read_csv('Contoso - Cadastro Produtos',sep=';')
-clientes_df = pd.read_csv('Contoso - Clientes',sep=';')
-
-print(vendas_df)
